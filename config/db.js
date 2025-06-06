0607fd059535104e409739de56859d864f485591
Ikram Lahmouri <ikram.lahmouri@my-digital-school.org>
2025-06-06 03:29:17 +0200

    correction

--- config/db.js
+++ config/db.js
@@ -7,8 +7,8 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,  // <-- modification ici
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT || 3306,
   multipleStatements: true
 });
 
