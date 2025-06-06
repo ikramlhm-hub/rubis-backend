c01c4bcc4a05f8cdf95c795ab77fdce75e083a1d
Ikram Lahmouri <ikram.lahmouri@my-digital-school.org>
2025-06-06 06:24:10 +0200

    FINAL

--- config/db.js
+++ config/db.js
@@ -7,11 +7,10 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,  // <-- ici DB_NAME au lieu de DB_DATABASE
   port: process.env.DB_PORT || 3306,
   multipleStatements: true
 });
-
 db.connect((err) => {
   if (err) {
     console.error('Connexion échouée :', err.message);
